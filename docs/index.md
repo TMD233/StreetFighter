e5ba9abc617bae1a40a2f38cd1069fd111eeedd4
Yueqi Jing <yjing4@vols.utk.edu>
2024-02-14 14:29:05 -0500

    code add

--- docs/index.md
+++ docs/index.md
@@ -14,3 +14,8 @@
 ## Features
 
 -   TODO
+
+
+## Test
+
+!{}
--- streetfighter/common.py
+++ streetfighter/common.py
@@ -5,3 +5,11 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+def random_number()
+    """Returns a random number between 0 to 1
+    Returns:
+        float: random number between 0 to 1
+    """
+    import random
+    return random_number
